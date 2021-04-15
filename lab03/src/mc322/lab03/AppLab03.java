b969cf79fda3cfbe039d3ecad5b3b381f2a36870
Wallace Lima <wallacegsantoslima@gmail.com>
2021-04-15 11:50:51 -0300

    adiciona AppLab03

---
+++ lab03/src/mc322/lab03/AppLab03.java
@@ -0,0 +1,20 @@
+package mc322.lab03;
+
+public class AppLab03
+{
+    public static void main(String[] args)
+    {
+        Animacao a1 = new Animacao("080403MCMVM");
+        System.out.println(a1.apresenta());
+        a1.passo();
+        System.out.println(a1.apresenta());
+        a1.passo();
+        System.out.println(a1.apresenta());
+        a1.passo();
+        System.out.println(a1.apresenta());
+        a1.passo();
+        System.out.println(a1.apresenta());
+        a1.passo();
+        System.out.println(a1.apresenta());
+    }
+}
