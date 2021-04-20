2f89774d881ff46cb45dfd0f44a232bec3001f82
Wallace Lima <wallacegsantoslima@gmail.com>
2021-04-20 16:53:09 -0300

    Faz o protótipo da classe Tabuleiro

---
+++ lab04/src/mc322/lab04/Tabuleiro.java
@@ -0,0 +1,32 @@
+package mc322.lab04;
+
+public class Tabuleiro
+{
+    Peca posicoes[][];
+    int num_pecas;
+
+    // Inicia um novo tabuleiro completo;
+    Tabuleiro()
+    {
+        posicoes = new Peca[7][7];
+        
+        num_pecas =  32;
+    }
+    // Realiza o movimento indicado, quando possível. Caso contrário, não faz nada.
+    public void mover(String inicio, String fim)
+    {
+
+    }
+
+    // Apresenta o estado atual do tabuleiro;
+    public void apresenta()
+    {
+
+    }
+
+    public String toString()
+    {
+        
+    }
+
+}
