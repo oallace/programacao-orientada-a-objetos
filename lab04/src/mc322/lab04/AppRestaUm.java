28a717dadf03c7030db19291cc32146db124b6a7
Wallace Lima <wallacegsantoslima@gmail.com>
2021-04-22 10:08:48 -0300

    Organiza o código

--- lab04/src/mc322/lab04/AppRestaUm.java
+++ lab04/src/mc322/lab04/AppRestaUm.java
@@ -8,7 +8,8 @@ public class AppRestaUm
         String resultado [];
         Tabuleiro tab = new Tabuleiro();
         CSVReader csv = new CSVReader();
-        
+        int iInicio, jInicio, iFim, jFim;
+
         // lê os comandos
         csv.setDataSource(caminhoCSV);
         comandos = csv.requestCommands();
@@ -17,11 +18,12 @@ public class AppRestaUm
         // executa os comandos da forma a9:a9
         for (int i = 0; i < comandos.length; i++)
         {
-            int iInicio, jInicio, iFim, jFim;
+            // converte o comando em string em seus valores inteiros:
             jInicio = Character.getNumericValue(comandos[i].charAt(0)) - Character.getNumericValue('a');
             iInicio = Character.getNumericValue(comandos[i].charAt(1)) - Character.getNumericValue('1');
             jFim = Character.getNumericValue(comandos[i].charAt(3)) - Character.getNumericValue('a');
             iFim = Character.getNumericValue(comandos[i].charAt(4)) - Character.getNumericValue('1');
+            // Realiza o movimento;
             tab.mover(iInicio, jInicio, iFim, jFim);
             tab.apresenta();
             resultado[i] = tab.toString();
@@ -32,6 +34,6 @@ public class AppRestaUm
 
     public static void main(String[] args)
     {
-        executaJogo("teste.csv");
+        executaJogo("testes/teste8.csv");
     }
 }
